9349a4646070ec99692d229d6e331231f8495384
Thiago Porto <thiago.porto@d3.do>
2022-06-16 17:50:49 -0300

    chore: first commit

---
+++ scss/src/scripts/build.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const sass = require("node-sass");
+
+const getComponents = () => {
+  let allComponents = [];
+
+  const types = ["atoms", "molecules"];
+
+  types.forEach((type) => {
+    const allFiles = fs
+      .readdirSync(path.resolve(`src/${type}`))
+      .map((file) => ({
+        input: `src/${type}/${file}`,
+        output: `src/lib/${file.slice(0, -4) + "css"}`,
+      }));
+
+    allComponents = [...allComponents, ...allFiles];
+  });
+
+  return allComponents;
+};
+
+const compile = (inputPath, output) => {
+  const result = sass.renderSync({
+    data: fs.readFileSync(path.resolve(inputPath)).toString(),
+    outputStyle: "expanded",
+    includePaths: [path.resolve("src")],
+    file: inputPath,
+  });
+
+  fs.writeFileSync(path.resolve(output), result.css.toString());
+};
+
+compile("src/global.scss", "src/lib/global.css");
+
+getComponents().forEach((component) => {
+  compile(component.input, component.output);
+});
